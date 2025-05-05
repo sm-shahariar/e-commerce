7f055839abd5329e8d2c65ed2dffec928aa37d3c
shahariarrahman1 <shahariarsfcit@gmail.com>
2025-05-06 01:20:00 +0600

    Update route for home view and modify Vite configuration for asset management

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,9 @@ use App\Http\Controllers\DashboardController;
 Route::get('/', [DashboardController::class, 'index']
 )->middleware(['auth', 'verified'])->name('dashboard');
 
-
+Route::get('/home', function() {
+    return view('frontend.home');
+});
 require __DIR__.'/auth.php';
 require __DIR__.'/admin.php';
 
--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,13 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/style.css', 'resources/js/script.js'],
             refresh: true,
         }),
     ],
+    build: {
+        outDir: 'public/build',
+        manifest: true,
+        emptyOutDir: true,
+    },
 });
