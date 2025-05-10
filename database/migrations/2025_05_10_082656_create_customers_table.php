a105fb23c76617edc4046f6481e05264a6e585c9
shahariarrahman1 <shahariarsfcit@gmail.com>
2025-05-10 18:28:37 +0600

    Add CartController and WishlistController for managing cart and wishlist functionalities Create customers migration with fields for name, email, phone, address, city, and zip Add customer_id foreign key to orders table for linking orders to customers

---
+++ database/migrations/2025_05_10_082656_create_customers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->nullable();
+            $table->foreignId('order_id')->constrained('orders')->onDelete('cascade');
+            $table->string('phone');
+            $table->string('address')->nullable();
+            $table->string('city')->nullable();
+            $table->string('zip')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customers');
+    }
+};
