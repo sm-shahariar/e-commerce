0d1b21be00526f11455de5247358800eaef5a7f0
shahariarrahman1 <shahariarsfcit@gmail.com>
2025-05-10 18:28:16 +0600

    Add variant_attribute_values migration and update order_items table to include variant_attribute_value_id

---
+++ database/migrations/2025_04_29_054410_variant_attribute_values_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('variant_attribute_values', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_attribute_value_id')->constrained('product_attribute_values')->onDelete('cascade');
+            $table->foreignId('product_attribute_id')->constrained('product_attributes')->onDelete('cascade');
+            $table->foreignId('product_variant_id')->constrained('product_variants')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_attribute_values_option');
+    }
+};
--- database/migrations/2025_04_29_060731_create_order_items_table.php
+++ database/migrations/2025_04_29_060731_create_order_items_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->foreignId('order_id')->constrained('orders')->onDelete('cascade');
             $table->foreignId('product_id')->constrained('products')->onDelete('cascade');
             $table->foreignId('product_variant_id')->nullable()->constrained('product_variants')->onDelete('cascade');
+            $table->foreignId('variant_attribute_value_id')->nullable()->constrained('variant_attribute_values')->onDelete('cascade');
             $table->integer('quantity')->default(1);
             $table->decimal('price', 10, 2);
             $table->timestamps();
